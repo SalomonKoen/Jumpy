39fb8068a6b19b0eb01f7d200d794ac1da2b46d0
SalomonKoen <salomonkoen@gmail.com>
2014-06-25 15:48:53 +0200

    Main Menu and partly settings menu

--- Eclipse/Jumpy/gen/com/example/jumpy/R.java
+++ Eclipse/Jumpy/gen/com/example/jumpy/R.java
@@ -25,12 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080000;
+        public static final int toggleButton1=0x7f080002;
+        public static final int txtSettings=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int main_menu=0x7f030000;
+        public static final int settings_menu=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -64,7 +66,6 @@ public final class R {
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
     }
