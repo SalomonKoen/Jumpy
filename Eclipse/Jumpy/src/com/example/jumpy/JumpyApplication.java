a1d94ef34eeb87028cfedb943f5af96a538935f4
SalomonKoen <salomonkoen@gmail.com>
2014-07-16 12:59:43 +0200

    JumpyApplication for "global variable"

---
+++ Eclipse/Jumpy/src/com/example/jumpy/JumpyApplication.java
@@ -0,0 +1,18 @@
+package com.example.jumpy;
+
+import android.app.Application;
+
+public class JumpyApplication extends Application
+{
+	private Player player;
+	
+	public Player getPlayer()
+	{
+		return player;
+	}
+	
+	public void setPlayer(Player player)
+	{
+		this.player = player;
+	}
+}
